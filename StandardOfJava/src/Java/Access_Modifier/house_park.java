95d96d404d289cfc9a93b1d7dd978e4275a9d7ff
jeong ha, Cho <cjh8487@naver.com>
2017-02-22 21:06:13 +0900

    17.02.22, cjh - wikidocs 자바 기

---
+++ StandardOfJava/src/Java/Access_Modifier/house_park.java
@@ -0,0 +1,5 @@
+package Java.Access_Modifier;
+
+public class house_park {
+	protected String lastname = "Kim";
+}
---
+++ StandardOfJava/src/Java/Access_Modifier/private_Accessor.java
@@ -0,0 +1,9 @@
+package Java.Access_Modifier;
+
+// private�� �ش� Ŭ���������� ������ �����ϴ�.
+public class private_Accessor {
+	private String secret;
+	private String getSecret(){
+		return this.secret;
+	}
+}
